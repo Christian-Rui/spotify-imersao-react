951047ccbc63dd08ccddc994aa04185fcff602a9
Christian-Rui <chris2w0social@gmail.com>
2025-02-02 03:49:29 -0300

    feat: adicionar assets,  componente header, e alterações no App

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./pages/Header/Header.js"
+
+function App() {
+  return (
+    <Header></Header>
+  );
+}
+
+export default App;
